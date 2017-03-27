355355560ce8fab54987d2c722de16d7e43ae695
root <root@localhost.localdomain>
2017-03-27 16:55:47 +0800

    Added specific shift time to email subject

--- Dashboard/send_mail.py
+++ Dashboard/send_mail.py
@@ -7,11 +7,18 @@ from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
 import smtplib
 from write_message import write_message
+from datetime import datetime, timedelta
+
 
 def _format_addr(s):
     name, addr = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
+now = datetime.now()
+earlier = now - timedelta(hours=12)
+from_date = str(earlier.year) + '/' + str(earlier.month) + '/' + str(earlier.day) + '-' + str(earlier.hour)
+to_date = str(now.year) + '/' + str(now.month) + '/' + str(now.day) + '-' + str(now.hour)
+	
 from_addr = 'PCT@EMCN.COM'
 to_addr = ['yi.li5@hpe.com', 'yuanfan.shi@hpe.com']
 
@@ -31,7 +38,7 @@ Table_C = write_message(Type = 3, Target = Target_C, From = 'HandoverTime', To =
 msg = MIMEText(Table_A + Table_B + Table_C, 'html', 'utf-8')
 msg['From'] = _format_addr('PCT Monitor <%s>' % from_addr)
 msg['To'] = _format_addr('管理员 <%s>' % to_addr)
-msg['Subject'] = Header('PCT Fail Report By Shift', 'utf-8').encode()
+msg['Subject'] = Header('PCT Fail Report By Shift (%s - %s)' % (from_date, to_date), 'utf-8').encode()
 
 server = smtplib.SMTP(smtp_server, 25)
 server.set_debuglevel(1)
